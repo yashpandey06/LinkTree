d11f0ed54ea515471e52804db045c9cf7b7c281d
Yash Pandey <pandeyyash7052@gmail.com>
2023-05-19 21:49:26 +0530

    Feat : update again the icon problem

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/bs'],
-    },
-  },
+
 
 })
