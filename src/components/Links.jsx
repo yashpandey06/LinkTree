41dc12a1453a01365cc1d55c45adceb94adb548f
Yash Pandey <pandeyyash7052@gmail.com>
2023-05-19 21:28:35 +0530

    Feat: Update vite.config file for deployment and re installed react-icons

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
 const Links = ({ links }) => {
-  console.log(links);
   return (
     <div className="flex flex-col gap-3 -mt-8 px-8 items-center">
       {links.map((item) => {
@@ -11,7 +10,11 @@ const Links = ({ links }) => {
             className="bg-gray-800  duration-200 hover:scale-105 border border--slate-50 flex flex-row justify-around items-center rounded-full p-2 max-w-md w-full"
           >
             <div>{item.img}</div>
-            <h2><a target="blank" href={item.link}>{item.name}</a></h2>
+            <h2>
+              <a target="blank" href={item.link}>
+                {item.name}
+              </a>
+            </h2>
           </div>
         );
       })}
